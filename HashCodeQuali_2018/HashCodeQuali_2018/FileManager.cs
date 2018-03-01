b4b7a70c1c24eec6abfc0a9d96c1116ce1a1762e
Dicrit <Dima.Bolsunov@gmail.com>
2018-03-01 19:20:46 +0200

    firs commit

---
+++ HashCodeQuali_2018/HashCodeQuali_2018/FileManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashCodeQuali_2018
+{
+    class FileManager
+    {
+        private static string currentDirectory = "C:\\Users\\Dell E6430u\\Documents\\Dima\\__HASHCODE";
+        private static string currentFile;
+
+
+        public static string[] getInputs()
+        {
+            return Directory.GetFiles(currentDirectory + "\\inputs", "*.in");
+        }
+
+
+        public static string getOutputFileName(string version = null)
+        {
+            
+            var str = string.Format("{0}\\outputs\\{2}\\{1}.out", currentDirectory, currentFile, version ?? ".");
+            if (version != null)
+                Directory.CreateDirectory(Path.GetDirectoryName(str));
+            return str;
+        }
+        public static StreamWriter createOutput(string version = null)
+        {
+            
+            return File.CreateText(getOutputFileName(version));
+        }
+        public static string[] getAllLines(int inputNumber)
+        {
+            string path = getInputs()[inputNumber];
+            currentFile = Path.GetFileNameWithoutExtension(path);
+            return File.ReadAllLines(path);
+        }
+
+        public static int[] GetIntArray(string str)
+        {
+            return Array.ConvertAll(str.Split(' '), num => Int32.Parse(num));
+        }
+        public static IEnumerable<int> GetIntIEnumerable(string str)
+        {
+            foreach (string s in str.Split(' '))
+            {
+                yield return Int32.Parse(s);
+            }
+        }
+    }
+}
---
+++ HashCodeQuali_2018/HashCodeQuali_2018/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashCodeQuali_2018
+{
+    class Program
+    {
+
+        static void Main()
+        {
+
+            Array.ForEach(FileManager.GetIntArray(FileManager.getAllLines(0)[0]), i =>
+            Console.WriteLine(i + " " + i.GetType()));
+            var writer = FileManager.createOutput();
+            writer.WriteLine("acdc");
+            writer.Close();
+            Console.ReadKey();
+        }
+    }
+}
