7ce9db922637f3b09bf17e4ca701816f21a3bf32
Dicrit <Dima.Bolsunov@gmail.com>
2018-03-01 19:45:15 +0200

    sad

--- HashCodeQuali_2018/HashCodeQuali_2018/FileManager.cs
+++ HashCodeQuali_2018/HashCodeQuali_2018/FileManager.cs
@@ -25,6 +25,8 @@ namespace HashCodeQuali_2018
             var str = string.Format("{0}\\outputs\\{2}\\{1}.out", currentDirectory, currentFile, version ?? ".");
             Directory.CreateDirectory(Path.GetDirectoryName(str));
             return str;
+
+
         }
         public static StreamWriter createOutput(string version = null)
         {
